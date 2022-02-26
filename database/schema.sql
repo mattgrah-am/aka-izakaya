8987a0a24665cf75cf0301f7ee9641d78d413c44
Stefan Fenech <stefan.fenech@live.com.au>
2022-02-26 16:12:42 +1100

    Update schema.sql
    Fixed schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(120),
     password VARCHAR(64),
-    admin BOOLEAN,
+    admin BOOLEAN
 );
 
 INSERT INTO users (email, password, admin) VALUES ('izakaya@gmail.com', 'izakaya', true);
@@ -12,7 +12,11 @@ CREATE TABLE menu(
     name VARCHAR(120),
     price INT,
     type VARCHAR(128),
-    category VARCHAR(128),
+    category VARCHAR(128)
 );
 
-INSERT INTO menu (name, price, type, category) VALUES ('Nikka', 19, 'Drink', 'Whisky')('Hakkaisan Tokubetsu junmai', 15, 'Drink', 'Sake')('Edemame', 6, 'Food', 'Entree')('Tuna Tartare', 15, 'Food', 'Entree');
+INSERT INTO menu (name, price, type, category) VALUES 
+('Nikka', 19, 'Drink', 'Whisky'),
+('Hakkaisan Tokubetsu junmai', 15, 'Drink', 'Sake'),
+('Edemame', 6, 'Food', 'Entree'),
+('Tuna Tartare', 15, 'Food', 'Entree');
