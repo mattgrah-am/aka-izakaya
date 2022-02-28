9172085dda9e272794887fd5d4e38f8c11a6c7d4
Matt Graham <iam@mattgrah.am>
2022-02-28 20:48:13 +1100

    push missing components

---
+++ client/js/components/contact.js
@@ -0,0 +1,10 @@
+function renderContact() {
+  const page = document.querySelector("#page");
+  const section = document.createElement("section");
+  section.classList.add("container");
+  page.replaceChildren(section);
+  section.innerHTML = `
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A magnam error soluta tempora dolorem quia quae, inventore voluptatum nostrum et fuga numquam aperiam, nisi, officiis placeat? Ut, tenetur ipsa. Accusantium?</p>
+  `;
+}
