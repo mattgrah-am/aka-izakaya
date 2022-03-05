637942254570ea3d7e467862aa52ee3245e16fcb
Stefan <stefan.fenech@live.com.au>
2022-03-05 19:26:22 +1100

    added login check for admin init file

--- client/admin/js/initializeAdmin.js
+++ client/admin/js/initializeAdmin.js
@@ -1,3 +1,23 @@
+function loginCheck() {
+    axios
+        .get('/api/session/')
+        .then((response) => {
+            const user = response.data.email;
 
+            renderAdminHeader(user);
+        })
+        .catch((error) => {
+            renderLogin();
+        });
+}
 
-renderLogin()
+// Check if there is already a user logged in.
+loginCheck();
+
+// If not, send to login page. If they are, render the admin page
+if (!user) {
+    renderLogin();
+} else {
+    renderAdminHeader();
+    renderAdminAbout();
+}
