7708cd47f9cc3020c6fe8c3c3201bb4ceed7c6bb
Matt Graham <iam@mattgrah.am>
2022-03-09 22:31:35 +1100

    fix login

--- client/admin/js/components/users/login.js
+++ client/admin/js/components/users/login.js
@@ -1,20 +1,22 @@
 function loginCheck() {
-    axios
-        .get('/api/session/')
-        .then((response) => {
-            const user = response.data.email;
-            renderAdminHeader(user);
-        })
-        .catch((error) => {
-            renderLogin();
-        });
+  axios
+    .get("/api/session/")
+    .then((response) => {
+      const user = response.data.email;
+      renderAdminHeader(user);
+    })
+    .catch((error) => {
+      renderLogin();
+    });
 }
 
 function renderLogin() {
-    const page = document.querySelector('#page');
-    const div = document.createElement('div');
-    page.replaceChildren(div);
-    div.innerHTML = `
+  const header = document.querySelector("#header-nav");
+  const page = document.querySelector("#page");
+  const div = document.createElement("div");
+  header.replaceChildren(div);
+  page.innerHTML = "";
+  div.innerHTML = `
     <div class="vh-100 vw-100 d-flex flex-column align-items-center mt-5">
         <p class="logo-font mb-0">Aka Izakaya</p>
         <div class="border border-dark p-4 rounded bg-light shadow">
@@ -36,38 +38,38 @@ function renderLogin() {
     </div>
   `;
 
-    const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        clearErrors();
-        const emailField = document.querySelector('#email');
-        const passwordField = document.querySelector('#password');
-        const body = {
-            email: emailField.value,
-            password: passwordField.value,
-        };
-        let error = null;
-        if (body.email === '') {
-            error = 'Email is required';
-        } else if (body.password === '') {
-            error = 'Password is required';
-        }
+  const loginForm = document.getElementById("login-form");
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    clearErrors();
+    const emailField = document.querySelector("#email");
+    const passwordField = document.querySelector("#password");
+    const body = {
+      email: emailField.value,
+      password: passwordField.value,
+    };
+    let error = null;
+    if (body.email === "") {
+      error = "Email is required";
+    } else if (body.password === "") {
+      error = "Password is required";
+    }
 
-        if (!error) {
-            axios
-                .post('/api/session/', body)
-                .then((response) => {
-                    console.log(response);
-                    renderAdminAbout();
-                    renderAdminHeader();
-                })
-                .catch((error) => {
-                    console.log(error.data);
-                    displayError(error.response.data.message);
-                });
-        } else {
-            console.log(error);
-            displayError(error);
-        }
-    });
+    if (!error) {
+      axios
+        .post("/api/session/", body)
+        .then((response) => {
+          console.log(response);
+          renderAdminAbout();
+          renderAdminHeader();
+        })
+        .catch((error) => {
+          console.log(error.data);
+          displayError(error.response.data.message);
+        });
+    } else {
+      console.log(error);
+      displayError(error);
+    }
+  });
 }
