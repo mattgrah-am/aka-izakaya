6a00673502e031c0418e06a6f73630dfb0db6c26
Matt Graham <32812400+mattgrah-am@users.noreply.github.com>
2022-03-12 16:52:27 +1100

    Update footer.js

--- client/js/components/footer.js
+++ client/js/components/footer.js
@@ -2,7 +2,7 @@ function renderFooter() {
   const footer = document.querySelector("#footer");
   footer.innerHTML = `
     <p class="text-light fw-light">Created by <a class="link-light text-decoration-none fw-bold" href="https://github.com/dalessi93" target="_blank" rel="noopener noreferrer">Daniel</a>,
-      <a class="link-light text-decoration-none fw-bold" href="https://github.com/BillyHoke" target="_blank" rel="noopener noreferrer">Stefan</a>, <a
+      <a class="link-light text-decoration-none fw-bold" href="https://github.com/stefan-fenech" target="_blank" rel="noopener noreferrer">Stefan</a>, <a
         class="link-light text-decoration-none fw-bold" href="https://github.com/tonesfrantz" target="_blank" rel="noopener noreferrer">Tony</a> & <a
         class="link-light text-decoration-none fw-bold" href="https://www.mattgrah.am/" target="_blank" rel="noopener noreferrer">Matt</a>. Code
       available for review on <a class="link-light text-decoration-none fw-bold" href="https://github.com/mattgrah-am/aka-izakaya" target="_blank"
